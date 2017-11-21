38f1465c1b471184973d764eb72860109c5688f1
Ryan Gifford <rgifford@solutionreach.com>
2017-11-21 12:54:54 -0700

    Test archive

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,5 @@
 docker run -i -v $PWD/tests:/go/ golang:latest /bin/bash -c \
 "
-rm *.xml && \
 go get -u github.com/jstemmer/go-junit-report && \
 go test -v 2>&1 | go-junit-report > report.xml && \
 touch report.xml
